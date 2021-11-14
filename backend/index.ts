7671f177f21932cc83ca62d24fe3101b465f4bd9
Alexander Törnvall <alexander.tornvall@icloud.com>
2021-11-14 21:25:33 +0100

    small refactoring

--- backend/index.ts
+++ backend/index.ts
@@ -10,31 +10,30 @@ const MongoClient = mongodb.MongoClient;
 
 const port = process.env.PORT || 8000
 
-if (process.env.RESTREVIEWS_DB_URI) {
-  MongoClient.connect(
-    process.env.RESTREVIEWS_DB_URI,
-    {
-      maxPoolSize: 50,
-      writeConcern: {
-        wtimeout: 2500,
-      },
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as mongodb.ConnectOptions
-  )
-
-    .then(async(_) => {
-      app.listen(port, () => {
-        console.log(`Listening on port ${port}`)
-      })
-    })
-
-    .catch((err) => {
-    console.log(err)
-    process.exit(1)
-  })
-
-} else {
-  console.error("Fatal: could not find .env variable... exiting")
+if (!process.env.RESTREVIEWS_DB_URI) {
+  console.log("Fatal: RESTREVIEWS_DB_URI not defined")
   process.exit(1)
 }
+
+MongoClient.connect(
+  process.env.RESTREVIEWS_DB_URI,
+  {
+    maxPoolSize: 50,
+    writeConcern: {
+      wtimeout: 2500,
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as mongodb.ConnectOptions
+)
+
+  .then(async(_) => {
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`)
+    })
+  })
+
+  .catch((err) => {
+  console.log(err)
+  process.exit(1)
+})
--- backend/server.ts
+++ backend/server.ts
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/restaurants", restaurants)
+
 // for all routes that don't exist (yet)
 app.use("*", (_, res) => res.status(404).json({ error: "Not found"}))
 
