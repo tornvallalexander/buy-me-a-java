55d6a88d12bb3e9e89d240b3dc81e6e81373bf1d
Alexander Törnvall <alexander.tornvall@icloud.com>
2021-11-27 13:16:36 +0100

    added heder to all current pages + small refactoring

---
+++ pages/profile.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Text } from "@chakra-ui/react";
+import Layout from "../components/Layout";
+
+const ProfileScreen = () => {
+  return (
+    <Layout>
+      <Text>profile page</Text>
+    </Layout>
+  );
+};
+
+export default ProfileScreen;
