5a71047b248ee54271d669edcafeb8577bd30bc6
Alexander Törnvall <alexander.tornvall@icloud.com>
2021-12-04 12:57:07 +0100

    added backend stripe integration for connecting accounts

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import hello from "./routes/hello.routes";
 import login from "./routes/login.routes";
 import signup from "./routes/login.routes";
+import connect from "./routes/connect.routes"
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/api/v1/auth/login", login);
 app.use("/api/v1/auth/signup", signup);
 app.use("/api/v1/greeting/hello", hello);
+app.use("/api/v1/connect", connect)
 
 // for all routes that don't exist (yet)
 app.use("*", (_, res) => res.status(404).json({ error: "Not found" }));
